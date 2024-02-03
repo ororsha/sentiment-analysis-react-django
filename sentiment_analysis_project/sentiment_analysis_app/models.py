ec792d6929c53a30264ec29059863f352c89dfcf
Or Sharabi <hpgresh@gmail.com>
2024-02-03 23:54:13 +0200

    saving data in database

--- sentiment_analysis_project/sentiment_analysis_app/models.py
+++ sentiment_analysis_project/sentiment_analysis_app/models.py
@@ -6,8 +6,9 @@ from django.db import models
 from django.db import models
 
 class UserInput(models.Model):
-    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    response = models.TextField()
 
     def __str__(self):
-        return self.text
+        return f'{self.text} - {self.response}'
--- sentiment_analysis_project/sentiment_analysis_app/views.py
+++ sentiment_analysis_project/sentiment_analysis_app/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .utils import analyze_sentiment, respond_to_sentiment
+from .models import UserInput
 
 @csrf_exempt
 def analyze_sentiment_api(request):
@@ -18,6 +19,11 @@ def analyze_sentiment_api(request):
         # Customize the response based on sentiment
         response_text = respond_to_sentiment(sentiment)
 
+        full_res = sentiment + " " + response_text;
+
+        # Save the user input to the database
+        save_user_input(text, full_res)
+
         # Optionally, you can log or perform additional processing here
 
         # Return the result as JSON
@@ -25,3 +31,11 @@ def analyze_sentiment_api(request):
     else:
         # Handle other HTTP methods if needed
         return JsonResponse({'error': 'Invalid request method'})
+
+
+def save_user_input(text, response):
+    # Create a UserInput instance
+    user_input = UserInput(text=text, response=response)
+
+    # Save the instance to the database
+    user_input.save()
